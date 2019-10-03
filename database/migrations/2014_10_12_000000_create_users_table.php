49c5d9746132a1209a91154419aaee21b2e70e64
Rus Hughes <russell.hughes@gmail.com>
2019-10-03 18:19:21 +0200

    woo some seeds!!

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('role_id');
+            $table->boolean('is_active')->default(true);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+        'role_id' => 1,
+        'is_active' => true,
+        'name' => 'Rus Hughes',
+        'email' => 'rus@gmail.com',
+        'password' => bcrypt('password'),
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ]);
+    }
+}
