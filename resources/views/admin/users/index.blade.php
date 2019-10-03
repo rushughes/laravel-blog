02555b421d52c5937311c950d37d045091839177
Rus Hughes <russell.hughes@gmail.com>
2019-10-03 22:27:02 +0200

    s27 l200

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Admin Users</div>
+
+                <div class="card-body">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @if(@users)
+                      @foreach($users as $user)
+                        <tr>
+                          <td>{{$user->id}}</td>
+                          <td>{{$user->name}}</td>
+                          <td>{{$user->email}}</td>
+                          <td>{{$user->created_at->diffForHumans()}}</td>
+                          <td>{{$user->updated_at->diffForHumans()}}</td>
+                        </tr>
+                        @endforeach
+                      @endif
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
