1a61a0143963b77af8b54adca872b4bd84ea2d49
Rus Hughes <russell.hughes@gmail.com>
2019-10-03 22:16:22 +0200

    s27 l198

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -13,7 +13,7 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.users.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.users.create');
     }
 
     /**
@@ -56,7 +56,7 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('admin.users.edit');
     }
 
     /**
